21b63818e4d1e8a6e03ba36aa702f701a42e4883
Timz-creator <timarleyf@gmail.com>
2026-03-11 22:35:22 +0000

    feat: finished init function

--- rover_env.py
+++ rover_env.py
@@ -1,6 +1,7 @@
 import gymnasium as gym
 import numpy as np
 import mujoco 
+from gymnasium import spaces
 
 class RoverEnv(gym.Env):
     def __init__(self):
@@ -9,9 +10,17 @@ class RoverEnv(gym.Env):
         self.model = mujoco.MjModel.from_xml_path("models/rover.xml")
 
         self.data = mujoco.MjData(self.model)
+
+        self.goal_pos = np.array([8.0, 0.0])
+        self.max_steps = 500
+        self.step_count = 0
+
+        self.action_space = spaces.Box(low=-1.0, high=1.0, shape=(2,), dtype=np.float32)
+
+        self.observation_space = spaces.Box(low= -np.inf, high=np.inf, shape=(20,), dtype=np.float32)
         
-        
-    
+
+
 
 
 
